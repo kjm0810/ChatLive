58ea2c52594a42847d35e9eb9d9561d1047b3c55
Your Name01~ <100568880+kjm0810@users.noreply.github.com>
2026-03-13 23:52:48 +0900

    style: container사이즈 추가

--- components/ChatHeader.tsx
+++ components/ChatHeader.tsx
@@ -8,24 +8,33 @@ function ChatHeader() {
   const { user, logout } = useAuth();
 
   return (
-    <div className='header'>
-      <div className="left">
-        <h1>
-          <Link href={'/'}>Chat Live</Link>
-        </h1>
-      </div>
-
-      {user && (
-        <div className="right">
-          <span className="nick">
-            {user.id}님
-          </span>
-          <div className="logout" onClick={logout}>
-            로그아웃 ↩
+      <div className='header'>
+        <div className="container header-inner">
+          <div className="left">
+            <h1>
+              <Link href={'/'}>Chat Live</Link>
+            </h1>
           </div>
+
+          {user && (
+            <div className="right">
+              <span className="nick">
+                {user.id}님
+              </span>
+              <div className="logout" onClick={logout}>
+                <span>
+                  로그아웃 
+                </span>
+                <svg viewBox="0 0 24 24" fill="none" stroke="#FFF">
+                  <polyline points="9 10 4 15 9 20"/>
+                  <path d="M20 4v7a4 4 0 0 1-4 4H4"/>
+                </svg>
+              </div>
+            </div>
+          )}
         </div>
-      )}
-    </div>
+        
+      </div>
   );
 }
 
